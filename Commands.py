d63f89f2ab2d2d412b9c03d766170aee30a3bd86
Kenny Baskett <kbaskett248@gmail.com>
2014-05-21 07:43:27 -0400

    Initial commit of EntitySelect package.
    This package is an extensible framework for performing actions based on the current selection. All code runs asynchronously after the selection is modified.
    
    The framework currently provides the following features:
    - Underlining an entity when it is selected if documentation is available. Pressing Alt + D opens the documentation
    - Highlighting matching entities by pressing Alt + H. Highlighted entities can be navigated.
    - Displaying a status message when an entity is selected.

---
+++ Commands.py
@@ -0,0 +1,256 @@
+import sublime
+import sublime_plugin
+
+from .src.EntitySelector import EntitySelector
+from .src.DocLink import DocLink
+from .src.Highlight import Highlight, PreemptiveHighlight
+
+try:
+    import sublimelogging
+    logger = sublimelogging.getLogger(__name__)
+except ImportError:
+    import logging
+    logger = logging.getLogger(__name__)
+    
+class EntitySelectListenerCommand(sublime_plugin.EventListener):
+
+    def on_selection_modified_async(self, view):
+        # logger.debug('Running on_modified')
+        EntitySelector.match_entity(view)
+
+    def on_activated_async(self, view):
+        # logger.debug('Running on_activated')
+        EntitySelector.match_entity(view)
+
+class DocLinkCommand(sublime_plugin.TextCommand):
+    """Command to find the documentation for the currently selected entity.
+
+    This command can be extended by defining new DocFinder classes.
+
+    """ 
+
+    def run(self, edit):
+        """Calls the show method of the DocFinder assigned to the view."""
+        try:
+            s = EntitySelector.get_selector_for_view(self.view)
+            if s.enable_doc_link():
+                s.show_doc()
+        except AttributeError:
+            pass
+
+    def description(self):
+        """Returns the description for the DocFinder assigned to the view."""
+        try:
+            es = EntitySelector.get_selector_for_view(self.view)
+            if es.enable_doc_link():
+                return es.doclink_description()
+        except AttributeError:
+            pass
+        return 'DocLink'
+
+    def is_visible(self):
+        """Returns true if the current file is an M-AT file."""
+        try:
+            for s in EntitySelector.get_possible_selectors_for_view(self.view):
+                if DocLink in s.__mro__:
+                    return True
+        except AttributeError:
+            pass
+        return False
+
+    def is_enabled(self):
+        """Returns True if a DocFinder is assigned to the view."""
+        try:
+            return EntitySelector.get_selector_for_view(self.view).enable_doc_link()
+        except AttributeError:
+            return False
+
+
+class EntityselectHighlightCommand(sublime_plugin.TextCommand):
+    """Command to find the documentation for the currently selected entity.
+
+    This command can be extended by defining new DocFinder classes.
+
+    """ 
+
+    def run(self, edit, cmd):
+        """Calls the show method of the DocFinder assigned to the view."""
+        if cmd == Highlight.HIGHLIGHT_COMMAND:
+            try:
+                s = EntitySelector.get_selector_for_view(self.view)
+                if s.enable_highlight():
+                    s.highlight()
+            except AttributeError:
+                pass
+        else:
+            hl = Highlight.get_highlighter_for_view(self.view)
+            if hl is None:
+                pass
+            elif cmd == Highlight.FORWARD_COMMAND:
+                hl.move_to_highlight(forward = True)
+            elif cmd == Highlight.BACKWARD_COMMAND:
+                hl.move_to_highlight(forward = False)
+            elif cmd == Highlight.CLEAR_COMMAND:
+                hl.remove_highlighter_from_view()
+            elif cmd == Highlight.SELECT_ALL_COMMAND:
+                hl.select_all_highlights()
+            elif cmd == Highlight.SHOW_ALL_COMMAND:
+                self.show_all(hl)
+
+    def description(self, cmd):
+        """Returns the description for the DocFinder assigned to the view."""
+        if cmd == Highlight.HIGHLIGHT_COMMAND:
+            try:
+                return EntitySelector.get_selector_for_view(self.view).highlight_description(cmd)
+            except AttributeError:
+                return 'Highlight'
+        else:
+            hl = Highlight.get_highlighter_for_view(self.view)
+            if hl is None:
+                return 'Highlight'
+            return hl.highlight_description(cmd)
+
+    def is_visible(self, cmd):
+        """Returns true if the current file is an M-AT file."""
+        if cmd == Highlight.HIGHLIGHT_COMMAND:
+            try:
+                for s in EntitySelector.get_possible_selectors_for_view(self.view):
+                    if Highlight in s.__mro__:
+                        return True
+            except AttributeError:
+                pass
+            return False
+        elif Highlight.get_highlighter_for_view(self.view) is None:
+            return False
+        else:
+            return True
+
+    def is_enabled(self, cmd):
+        """Returns True if a Highlighter is assigned to the view."""
+        if cmd == Highlight.HIGHLIGHT_COMMAND:
+            try:
+                return EntitySelector.get_selector_for_view(self.view).enable_highlight()
+            except AttributeError:
+                return False
+        elif Highlight.get_highlighter_for_view(self.view) is None:
+            return False
+        else:
+            return True
+
+    def show_all(self, highlighter):
+        items = highlighter.highlight_regions
+        disp_items = [highlighter.get_display_region(r) for r in items]
+        curr_sel = [s for s in self.view.sel()]
+        curr_vp = self.view.viewport_position()
+        func = lambda x: self.show_error(x, items, curr_sel, curr_vp)
+        self.view.window().show_quick_panel(disp_items,
+                                            func,
+                                            0,
+                                            0,
+                                            func)
+
+    def show_error(self, index, items, curr_sel, curr_vp):
+        if index == -1:
+            self.view.sel().clear()
+            self.view.sel().add_all(curr_sel)
+            self.view.set_viewport_position(curr_vp, True)
+        else:
+            reg = items[index]
+            self.view.sel().clear()
+            self.view.sel().add(reg)
+            self.view.show_at_center(reg)
+
+
+class PreemptiveHighlightCommand(sublime_plugin.TextCommand):
+    """Command to find the documentation for the currently selected entity.
+
+    This command can be extended by defining new DocFinder classes.
+
+    """ 
+
+    def run(self, edit, highlighter):
+        """Calls the show method of the DocFinder assigned to the view."""
+        c = PreemptiveHighlight.get_preemptive_highlighter(highlighter)
+        if c is not None:
+            ps = c.get_preemptive_highlight_selection(self.view)
+            if not ps:
+                sublime.status_message('No regions to highlight')
+                return
+
+            # Copy current selection, then replace with preemptive selection
+            current_selection = self.copy_current_selection()
+            self.view.sel().clear()
+            self.view.sel().add_all(ps)
+
+            # Get current selector
+            current_selector = EntitySelector.get_selector_for_view(self.view)
+
+            # Determine if the highlighter should be enabled
+            enabled = c.enable_for_selection(self.view)
+            if not enabled:
+                sublime.status_message('No regions to highlight')
+                self.view.sel().clear()
+                self.view.sel().add_all(current_selection)
+                return
+
+            # Create the highlighter and highlight
+            preemptive_highlighter = c(self.view, **enabled)
+            preemptive_highlighter.highlight()
+
+            # Restore the current selection
+            self.view.sel().clear()
+            self.view.sel().add_all(current_selection)
+
+            # If there were no highlighted regions, remove the highlight
+            if not preemptive_highlighter.highlight_regions:
+                sublime.status_message('No regions to highlight')
+                preemptive_highlighter.remove_highlighter_from_view()
+
+            # Restore the current selector
+            if current_selector is None:
+                EntitySelector.update_selector_for_view(self.view, None)
+            else:
+                current_selector.__class__.update_selector_for_view(self.view, current_selector)
+
+    def copy_current_selection(self):
+        return [r for r in self.view.sel()]
+
+    def is_visible(self, highlighter):
+        """Returns true if the current file is an M-AT file."""
+        if PreemptiveHighlight.get_preemptive_highlighter(highlighter) is not None:
+            return True
+        return False
+
+    def is_enabled(self, highlighter):
+        """Returns True if a DocFinder is assigned to the view."""
+        return self.is_visible(highlighter)
+
+
+class HighlightListenerCommand(sublime_plugin.EventListener):
+    """Refreshes the highlights on activation and modification of the view."""
+
+    def on_activated_async(self, view):
+        self.update_highlights(view)
+
+    def on_modified_async(self, view):
+        self.update_highlights(view)
+
+    def update_highlights(self, view):
+        if view.settings().get('is_widget', False):
+            return
+        hl = Highlight.get_highlighter_for_view(view)
+        if hl is not None:
+            hl.highlight()
+            if not hl.highlight_regions:
+                hl.remove_highlighter_from_view()
+        else:
+            view.erase_regions('entity_select_highlight')
+
+class EntitySelectInsertInViewCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit, text, point = 0):
+
+        self.view.insert(edit, point, text)
+
+
+
---
+++ src/DocLink.py
@@ -0,0 +1,154 @@
+from abc import ABCMeta, abstractmethod
+import os
+import time
+import webbrowser
+
+import sublime
+
+from .EntitySelector import EntitySelector, SortableABCMeta
+
+try:
+    import sublimelogging
+    logger = sublimelogging.getLogger(__name__)
+except ImportError:
+    import logging
+    logger = logging.getLogger(__name__)
+
+class DocLink(EntitySelector):
+
+    def __init__(self, view, search_string = None, search_region = None, **kwargs):
+        if search_region is not None:
+            if DocLink.selection_in_region(view, search_region):
+                super(DocLink, self).__init__(view, search_string = search_string, 
+                                              search_region = search_region, 
+                                              **kwargs)
+                if (search_string is None):
+                    self.search_string = view.substr(search_region)
+                else:
+                    self.search_string = search_string
+                self.regions = [search_region]
+            else:
+                logger.warning('Selection not in Region: %s - %s', search_region, search_string)
+        else:
+            super(DocLink, self).__init__(view, search_string = search_string, 
+                                          search_region = search_region, 
+                                          **kwargs)
+            self.regions = [search_region]
+            self.search_string = search_string
+
+    @abstractmethod
+    def show_doc(self):
+        """This is called to show the documentation for an entity."""
+        pass
+
+    @staticmethod
+    def selection_in_region(view, region):
+        """Return True if any view selections are in the region."""
+        for s in view.sel():
+            if region.contains(s):
+                return True
+        return False
+
+    def doclink_description(self):
+        """Return a description string to display in the context menu."""
+        try:
+            if self.search_string:
+                return 'DocLink: %s' % self.search_string
+        except AttributeError:
+            pass
+        
+        return 'DocLink'
+
+    @property
+    def open_status_message(self):
+        """Status message displayed when opening documentation."""
+        return "DocLink: " + self.search_string
+
+    @staticmethod
+    def add_regions(view = None, selector = None, **kwargs):
+        """Adds regions to the view and assigns the DocFinder to the view."""
+        if ((view is not None) and (selector is not None) and 
+            isinstance(selector, DocLink) and selector.enable_doc_link()):
+            view.add_regions('doc_link', selector.regions, 
+                             view.scope_name(selector.regions[0].begin()),
+                             flags = (sublime.DRAW_NO_FILL | 
+                                      sublime.DRAW_NO_OUTLINE |
+                                      sublime.DRAW_STIPPLED_UNDERLINE |
+                                      sublime.HIDE_ON_MINIMAP)
+                         )
+    
+    @staticmethod
+    def erase_regions(view = None, **kwargs):
+        """Clears regions from the view and clears the DocFinder assigned to the view."""
+        if view is not None:
+            view.erase_regions('doc_link')
+    
+    def enable_doc_link(self):
+        """Return True to allow DocLink functionality for the EntitySelector."""
+        return True
+
+    def show_doc_on_web(self, url):
+        """Opens the given url in the default web browser."""
+        webbrowser.open(url)
+
+    def show_doc_in_file(self, file_, region = None, row = 0, col = 0, show_at_top = True):
+        """Opens the file and shows the given region."""
+        logger.debug('show_doc_in_file')
+        status_message_suffix = ''
+
+        if (file_ is None):
+            status_message_suffix = 'not found'
+        elif (file_ == self.view.file_name()):
+            logger.debug('in current file')
+            self.view.show(region, True)
+            self.view.sel().clear()
+            self.view.sel().add(region)
+            if show_at_top:
+               line = self.view.line(region)
+               line = self.view.line(line.begin() - 1)
+               v = self.view.text_to_layout(line.begin())
+               self.view.set_viewport_position(v, True)
+            status_message_suffix = 'found in current file'
+        elif not os.path.exists(file_):
+            status_message_suffix = 'not found'
+        else:
+            status_message_suffix = 'found in other file'
+            if (region is None):
+                view = self.view.window().open_file(
+                            "{0}:{1}:{2}".format(file_, row, col),
+                             sublime.ENCODED_POSITION)
+                if ((row != 0) and (col != 0)):
+                    sublime.set_timeout_async(
+                        lambda: self.show_and_select_opened_file(view, region, 
+                                    row, col, show_at_top), 0)
+            else:
+                view = self.view.window().open_file(file_, sublime.ENCODED_POSITION)
+                sublime.set_timeout_async(
+                    lambda: self.show_and_select_opened_file(view, region, 
+                                row, col, show_at_top), 0)
+
+        return status_message_suffix
+
+    def show_and_select_opened_file(self, view, region, row, col, show_at_top):
+        """Helper method to show the given selection in a just opened file."""
+        while view.is_loading():
+            time.sleep(0.01)
+
+        s = view.sel()
+        s.clear()
+        if region is not None:
+            s.add(region)
+            line = view.line(region)
+        else:
+            p = view.text_point(row-1, col-1)
+            line = view.line(p)
+            selection = sublime.Region(p, line.end())
+            s.add(selection)
+
+        if show_at_top:
+            previous_line = view.line(line.begin()-1)
+            v = view.text_to_layout(previous_line.begin())
+            view.set_viewport_position(v, False)
+
+DocLink.add_on_before_check_callback(DocLink.erase_regions) 
+DocLink.add_on_after_check_callback(DocLink.add_regions)
